8c2e943adae1983471637722b0d3628ce0bde311
mijabi <mijabi@gmail.com>
2014-12-29 14:49:33 +0900

    remove comments a little bit

--- Gruntfile.js
+++ Gruntfile.js
@@ -18,46 +18,31 @@ module.exports = function (grunt) {
       ]
     },
  
-    // ローカルサーバー設定
     connect: {
       options: {
       },
       livereload: {
         options: {
-          base: 'dev/',// rootを設定
+          base: 'dev/',// set root
           open: true
         }
       }
     },
+
     compass: {
-/*      dist: {
-        options: {
-        sassDir: 'sass',
-        cssDir: 'css',
-        environment: 'production'
-        }
-      },*/
       dev: {
         options: {
           config: 'config.rb'
         }
       }
-/*      dev: {                    // Another target
-        options: {
-          sassDir: 'develop/sass',
-          cssDir: 'develop/css'
-        }
-      }*/
     },
 
     jshint: {
       options: {
         jshintrc: '.jshintrc'
       }
-//      },
-//      all: ['develop/js/*.js']
     },
-    // 公開用にdistフォルダにコピー
+
     copy: {
       dist: {
         expand: true,
@@ -66,48 +51,32 @@ module.exports = function (grunt) {
         src: '**'
       }
     },
-    // 公開フォルダから不要なデータを削除
+
     clean: {
       build: ['dist/sass']
     },
 
     concat: {
         options: {
-            // 結合したファイルの頭にコメント入れたいとき
-            banner: '<%= general.comment %>'
+            banner: '<%= general.comment %>' // leave comment
         },
         files: {
-            // 出力元
             src: [
                 'dev/js/a.js',
                 'dev/js/b.js'
             ],
-            // 出力先( package.jsonのnameから値を取得している)
             dest: 'dev/js/<%= pkg.name %>.js'
         }
     },
 
-
-/*
     uglify: {
-      my_target: {
-        files: {
-          expand: true,     // 動的拡張機能を有効
+      options: {
+/*
 //          beautify: true, 
           preserveComments: 'some', // all で全てのコメントを保持、some で /?! から始めるコメントのみ保持
 //          report: 'gzip', // gzipファイル生成
           sourceMap: true, 
-          src: './js/*.js', // マッチする実際のパターン
-          dest: '../js/',   // 遷移先のパスの先頭部分
-          ext: '.min.js',   // 遷移先のファイルパスにつける拡張子
-        }
-      }
-    },
 */
-
-
-    uglify: {
-      options: {
         banner: '<%= general.comment %>',
         mangle: true
       },
